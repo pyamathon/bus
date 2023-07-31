e2b40c605776711b60a8adc0181205ec1a0354b3
pyamathon <140927851+pyamathon@users.noreply.github.com>
2023-07-31 09:14:42 +0900

    Add files via upload

--- bus.py
+++ bus.py
@@ -74,7 +74,8 @@ def main_page():
 
     column_list = st.session_state.df0.columns[3:].values
     #column_list_selector = st.sidebar.multiselect("停留所", column_list, default = column_list)
-    index_selector = st.sidebar.multiselect("出発時間", st.session_state.df0["始発時分"], default = st.session_state.df0["始発時分"], key = "time", on_change = change_around_time)
+    df_list('始発時分')
+    index_selector = st.sidebar.multiselect("出発時間", st.session_state.df_list, default = st.session_state.df_list, key = "time", on_change = change_around_time)
 
     #サイドバー作成用
     #[6,7,8]時台
